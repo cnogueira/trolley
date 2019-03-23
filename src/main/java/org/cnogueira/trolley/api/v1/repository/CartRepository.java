eb2bc8994c5721eaf87ad3a323020de2c854960b
Cris Nogueira <cristofor.nogueira@gmail.com>
2019-03-23 13:06:56 +0100

    Add missing @Repository annotation to make CartRepository eligible for bean injection

--- src/main/java/org/cnogueira/trolley/api/v1/repository/CartRepository.java
+++ src/main/java/org/cnogueira/trolley/api/v1/repository/CartRepository.java
@@ -1,7 +1,9 @@
 package org.cnogueira.trolley.api.v1.repository;
 
 import org.cnogueira.trolley.api.v1.dto.Cart;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class CartRepository {
     public void addCart(final Cart cart) {
     }
