6e96cd8d76cc272cc1895251aa0056b04f3ed3ef
Cris Nogueira <cristofor.nogueira@gmail.com>
2019-03-23 12:26:28 +0100

    Added failing tests for CartController.createCart

---
+++ src/main/java/org/cnogueira/trolley/api/v1/controller/CartController.java
@@ -0,0 +1,5 @@
+package org.cnogueira.trolley.api.v1.controller;
+
+public class CartController {
+
+}
---
+++ src/main/java/org/cnogueira/trolley/api/v1/service/CartService.java
@@ -0,0 +1,13 @@
+package org.cnogueira.trolley.api.v1.service;
+
+import org.cnogueira.trolley.api.v1.dto.Cart;
+import org.cnogueira.trolley.api.v1.dto.CartCreateRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartService {
+
+    public Cart createCart(CartCreateRequest cartCreateRequest) {
+        return null;
+    }
+}
