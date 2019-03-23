acee6ba2b636d25b8e45acd9ccad71bb6a527d3b
Cris Nogueira <cristofor.nogueira@gmail.com>
2019-03-23 10:52:42 +0100

    Initial commit after spring initializr scaffolding

---
+++ src/main/java/org/cnogueira/trolley/TrolleyApplication.java
@@ -0,0 +1,13 @@
+package org.cnogueira.trolley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrolleyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrolleyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cnogueira/trolley/TrolleyApplicationTests.java
@@ -0,0 +1,16 @@
+package org.cnogueira.trolley;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrolleyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
