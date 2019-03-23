795617c2cdcaf002971422da747588b4af2c63ab
Cris Nogueira <cristofor.nogueira@gmail.com>
2019-03-23 19:08:51 +0100

    Add failing tests for ItemRepository.addItem

--- src/main/java/org/cnogueira/trolley/api/v1/repository/ItemRepository.java
+++ src/main/java/org/cnogueira/trolley/api/v1/repository/ItemRepository.java
@@ -3,9 +3,16 @@ package org.cnogueira.trolley.api.v1.repository;
 import org.cnogueira.trolley.api.v1.domain.Item;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public class ItemRepository {
     public void addItem(final Item item) {
 
+    }
+
+    public Optional<Item> getById(final UUID itemId) {
+        return  null;
     }
 }
