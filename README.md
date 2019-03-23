9f4accd55791dd03e210c4b4bc66c41ece2475f7
Cris Nogueira <cristofor.nogueira@gmail.com>
2019-03-23 15:28:49 +0100

    Add CI and static analysis tools

---
+++ README.md
@@ -0,0 +1,13 @@
+[![Build Status](https://travis-ci.com/cnogueira/trolley.svg?branch=master)](https://travis-ci.com/cnogueira/trolley) [![Codacy Badge](https://api.codacy.com/project/badge/Grade/5a878d3468a74227a94300395a6dfdb3)](https://www.codacy.com/app/cnogueira_2/trolley?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=cnogueira/trolley&amp;utm_campaign=Badge_Grade)
+
+# Trolley 
+
+Yet Another Cart Manager application
+
+
+#### Instructions
+
+Run tests with
+```
+./gradlew clean check
+```
--- src/main/java/org/cnogueira/trolley/api/v1/controller/CartController.java
+++ src/main/java/org/cnogueira/trolley/api/v1/controller/CartController.java
@@ -1,12 +1,10 @@
 package org.cnogueira.trolley.api.v1.controller;
 
 import lombok.AllArgsConstructor;
-import lombok.val;
 import org.cnogueira.trolley.api.v1.dto.Cart;
 import org.cnogueira.trolley.api.v1.dto.CartCreateRequest;
 import org.cnogueira.trolley.api.v1.service.CartService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
