26f94db945de78e5893baebafff760f274c5930b
Cris Nogueira <cristofor.nogueira@gmail.com>
2019-03-23 12:34:03 +0100

    Provided passing implementation for CartController.createCart

--- src/main/java/org/cnogueira/trolley/api/v1/controller/CartController.java
+++ src/main/java/org/cnogueira/trolley/api/v1/controller/CartController.java
@@ -1,5 +1,28 @@
 package org.cnogueira.trolley.api.v1.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.val;
+import org.cnogueira.trolley.api.v1.dto.Cart;
+import org.cnogueira.trolley.api.v1.dto.CartCreateRequest;
+import org.cnogueira.trolley.api.v1.service.CartService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(path = "/api/v1/carts")
 public class CartController {
 
+    private final CartService cartService;
+
+    @PostMapping
+    public ResponseEntity<Cart> createCart(@RequestBody CartCreateRequest cartCreateRequest) {
+        val cart = cartService.createCart(cartCreateRequest);
+
+        return new ResponseEntity<>(cart, HttpStatus.CREATED);
+    }
 }
