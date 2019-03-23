ccf54b4f1b53fd2a3d926fc607758806f75e8fb4
Cris Nogueira <cristofor.nogueira@gmail.com>
2019-03-23 19:10:49 +0100

    Add passing implementation for ItemRepository.addItem. Also Acceptance tests green

--- src/main/java/org/cnogueira/trolley/api/v1/repository/ItemRepository.java
+++ src/main/java/org/cnogueira/trolley/api/v1/repository/ItemRepository.java
@@ -3,16 +3,21 @@ package org.cnogueira.trolley.api.v1.repository;
 import org.cnogueira.trolley.api.v1.domain.Item;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public class ItemRepository {
-    public void addItem(final Item item) {
 
+    private final Map<UUID, Item> items = new HashMap<>();
+
+    public void addItem(final Item item) {
+        items.put(item.getId(), item);
     }
 
     public Optional<Item> getById(final UUID itemId) {
-        return  null;
+        return  Optional.ofNullable(items.get(itemId));
     }
 }
