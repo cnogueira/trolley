5c53ae1dfeb355d33400a9c7886c1918c1f6e8e0
Cris Nogueira <cristofor.nogueira@gmail.com>
2019-03-23 13:01:09 +0100

    Add failing tests for CartService.createCart

---
+++ src/main/java/org/cnogueira/trolley/api/v1/repository/CartRepository.java
@@ -0,0 +1,8 @@
+package org.cnogueira.trolley.api.v1.repository;
+
+import org.cnogueira.trolley.api.v1.dto.Cart;
+
+public class CartRepository {
+    public void addCart(Cart cart) {
+    }
+}
--- src/main/java/org/cnogueira/trolley/api/v1/service/CartService.java
+++ src/main/java/org/cnogueira/trolley/api/v1/service/CartService.java
@@ -1,11 +1,16 @@
 package org.cnogueira.trolley.api.v1.service;
 
+import lombok.RequiredArgsConstructor;
 import org.cnogueira.trolley.api.v1.dto.Cart;
 import org.cnogueira.trolley.api.v1.dto.CartCreateRequest;
+import org.cnogueira.trolley.api.v1.repository.CartRepository;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CartService {
+
+    private final CartRepository cartRepository;
 
     public Cart createCart(CartCreateRequest cartCreateRequest) {
         return null;
