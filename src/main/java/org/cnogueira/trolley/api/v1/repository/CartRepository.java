b4f7a02965d4c9027bede839cff9916d1015b9bf
Cris Nogueira <cristofor.nogueira@gmail.com>
2019-03-23 16:19:33 +0100

    Add passing implementation for CartRepository and get-cart acceptance test

--- src/main/java/org/cnogueira/trolley/api/v1/repository/CartRepository.java
+++ src/main/java/org/cnogueira/trolley/api/v1/repository/CartRepository.java
@@ -1,5 +1,7 @@
 package org.cnogueira.trolley.api.v1.repository;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 import org.cnogueira.trolley.api.v1.dto.Cart;
@@ -7,10 +9,14 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class CartRepository {
+
+    private final Map<UUID, Cart> carts = new HashMap<>();
+
     public void addCart(final Cart cart) {
+        carts.put(cart.getId(), cart);
     }
 
-    public Optional<Cart> getById(UUID eq) {
-        return null;
+    public Optional<Cart> getById(final UUID cartId) {
+        return Optional.ofNullable(carts.get(cartId));
     }
 }
