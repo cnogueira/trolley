b425f5f30ff0b4bdfa365a72b4adecff06c645d2
Cris Nogueira <cristofor.nogueira@gmail.com>
2019-03-24 10:55:53 +0100

    Add failing test to ensure Cart notifies observers whenever an item is added to it

--- src/main/java/org/cnogueira/trolley/api/v1/service/stateChange/StateChangeNotifier.java
+++ src/main/java/org/cnogueira/trolley/api/v1/service/stateChange/StateChangeNotifier.java
@@ -1,4 +1,9 @@
 package org.cnogueira.trolley.api.v1.service.stateChange;
 
+import org.cnogueira.trolley.api.v1.domain.Cart;
+
 public class StateChangeNotifier {
+    public void notifyStateChanged(final Cart emitter) {
+
+    }
 }
--- src/test/java/org/cnogueira/trolley/api/v1/domain/CartTest.java
+++ src/test/java/org/cnogueira/trolley/api/v1/domain/CartTest.java
@@ -2,7 +2,7 @@ package org.cnogueira.trolley.api.v1.domain;
 
 import lombok.val;
 import org.cnogueira.trolley.api.v1.domain.factory.CartFactory;
-import org.cnogueira.trolley.api.v1.dto.CartCreateRequest;
+import org.cnogueira.trolley.api.v1.service.stateChange.StateChangeNotifier;
 import org.cnogueira.trolley.api.v1.service.stateChange.StateChangeNotifierFactory;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,6 +11,11 @@ import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CartTest {
@@ -23,24 +28,6 @@ public class CartTest {
     @Before
     public void setUp() {
         cartFactory = new CartFactory(stateChangeNotifierFactory);
-    }
-
-    @Test
-    public void from_CartGetsCreatedWithZeroItems() {
-        // when
-        val cart = cartFactory.from(CartCreateRequest.withName("abc"));
-
-        // then
-        assertThat(cart.getItems()).isEmpty();
-    }
-
-    @Test
-    public void withName_CartGetsCreatedWithZeroItems() {
-        // when
-        val cart = cartFactory.with("cart 1");
-
-        // then
-        assertThat(cart.getItems()).isEmpty();
     }
 
     @Test
@@ -75,5 +62,20 @@ public class CartTest {
         // then
         assertThat(cart.getItems()).hasSize(1);
         assertThat(cart.getItems()).contains(item);
+    }
+
+    @Test
+    public void addItem_notifiesStateChangeObservers() {
+        // given
+        val stateChangeNotifier = mock(StateChangeNotifier.class);
+        given(stateChangeNotifierFactory.createStateChangeNotifier()).willReturn(stateChangeNotifier);
+        val cart = cartFactory.with("cart with StateChangeNotifier spy");
+        val item = Item.withName("just an item");
+
+        // then
+        cart.addItem(item);
+
+        // when
+        verify(stateChangeNotifier, times(1)).notifyStateChanged(same(cart));
     }
 }
