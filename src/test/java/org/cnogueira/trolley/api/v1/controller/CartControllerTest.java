6bde1ea74cb9a23a5616a539fb6ba99b410234b8
Cris Nogueira <cristofor.nogueira@gmail.com>
2019-03-26 23:10:54 +0100

    Add rest-docs

--- src/test/java/org/cnogueira/trolley/api/v1/controller/CartControllerTest.java
+++ src/test/java/org/cnogueira/trolley/api/v1/controller/CartControllerTest.java
@@ -11,6 +11,7 @@ import org.cnogueira.trolley.api.v1.service.CartService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
@@ -24,6 +25,9 @@ import java.util.UUID;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -31,6 +35,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(CartController.class)
+@AutoConfigureRestDocs(outputDir = "api-docs")
 public class CartControllerTest {
 
     private static final String CARTS_API = "/api/v1/carts";
@@ -49,7 +54,7 @@ public class CartControllerTest {
     @Test
     public void createCart_delegatesToCartService() throws Exception {
         // given
-        val cartRequestBody = CartCreateRequest.withName("a name for the cart");
+        val cartRequestBody = CartCreateRequest.withName("cart 1");
         val cart = cartFactory.from(cartRequestBody);
         given(cartService.createCart(eq(cartRequestBody))).willReturn(cart);
 
@@ -61,7 +66,12 @@ public class CartControllerTest {
         // then
         response.andExpect(status().is(HttpStatus.CREATED.value()))
                 .andExpect(jsonPath("id").value(cart.getId().toString()))
-                .andExpect(jsonPath("name").value(cartRequestBody.getName()));
+                .andExpect(jsonPath("name").value(cartRequestBody.getName()))
+                .andDo(document("create-cart", responseFields(
+                    fieldWithPath("id").description("Id of the created cart"),
+                    fieldWithPath("name").description("Name of the created cart"),
+                    fieldWithPath("items").description("List of items included in this cart")
+                )));
     }
 
     @Test
@@ -76,7 +86,13 @@ public class CartControllerTest {
         // then
         response.andExpect(status().is(HttpStatus.OK.value()))
                 .andExpect(jsonPath("id").value(cart.getId().toString()))
-                .andExpect(jsonPath("name").value(cart.getName()));
+                .andExpect(jsonPath("name").value(cart.getName()))
+                .andExpect(jsonPath("items").isArray())
+                .andDo(document("get-cart", responseFields(
+                    fieldWithPath("id").description("Id of the cart"),
+                    fieldWithPath("name").description("Name of the cart"),
+                    fieldWithPath("items").description("List of items added to this cart")
+                )));
     }
 
     @Test
@@ -119,7 +135,11 @@ public class CartControllerTest {
         // then
         response.andExpect(status().is(HttpStatus.CREATED.value()))
             .andExpect(jsonPath("id").value(item.getId().toString()))
-            .andExpect(jsonPath("name").value(itemAddRequest.getName()));
+            .andExpect(jsonPath("name").value(itemAddRequest.getName()))
+            .andDo(document("add-item", responseFields(
+                fieldWithPath("id").description("Id of the created item"),
+                fieldWithPath("name").description("Name of the item")
+            )));
     }
 
     private MockHttpServletRequestBuilder addItemPostRequest(final UUID cartId,
